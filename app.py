19033a99073d1e0f0984ef13c84945ab0d98331f
George Trigonakis <gtrig84@gmail.com>
2021-03-13 01:27:53 +0200

    added configuration files

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
 from nut import nutPullData
 from datetime import datetime
 import time
+import configparser
 
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-# You can generate a Token from the "Tokens Tab" in the UI
-token = "influxtoken"
-org = "none"
-bucket = "ups_data"
+server_config = configparser.ConfigParser()
+server_config.read("./nut_servers.ini")
+general_config = configparser.ConfigParser()
+general_config.read("./config.ini")
 
-client = InfluxDBClient(url="http://192.168.8.200:8086", token=token)
+#Influx configuration loading
+influx_url = general_config.get('influx','url')
+token = general_config.get('influx','token')
+org = general_config.get('influx','org')
+bucket = general_config.get('influx','bucket')
+
+client = InfluxDBClient(url=influx_url, token=token)
 
 while True:
-    ups_data = [
-        nutPullData(host='192.168.8.200',port='3493',username='monuser',password='pass'),
-        nutPullData(host='192.168.8.180',port='3493',username='monuser',password='pass')
-    ]
+    ups_data = []
+    for server in server_config.sections():
+        ups_data.append(nutPullData(host=server_config.get(server,'host'),port=server_config.get(server,'port'),username=server_config.get(server,'username'),password=server_config.get(server,'password')))
+        
         
     power_ok=True
     write_api = client.write_api(write_options=SYNCHRONOUS)
 
     for ups in ups_data:
+        if ups == False: 
+            continue
         if ups['ups.status']!='OL' or ups['battery.charge']<100 :
             power_ok=False
         
@@ -39,6 +48,6 @@ while True:
             print('Couldnt send data to influx')
     
     if power_ok:
-        time.sleep(30)
+        time.sleep(general_config.getint('timing','OK_INTERVAL'))
     else:
-        time.sleep(1)
+        time.sleep(general_config.getint('timing','NOT_OK_INTERVAL'))
